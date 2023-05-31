166831e5e4723d126b9aea87fac02723973d2a44
Zenyet <zenyet1998@gmail.com>
2023-06-01 00:44:42 +0800

    custom mono font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
             colors: { // colors 如果定义在extend的外部，会导致原本的预设颜色都失效没有
                 "ter-top": "#353534",
                 "ter-border": "#777776",
-                "triangle": "#0073cc"
+                "triangle": "#0073cc",
             },
             animation: {
                 breath: "breath 1s ease-in-out infinite"
@@ -19,6 +19,9 @@ export default {
                     '0%': {opacity: 1},
                     '100%': {opacity: 0}
                 }
+            },
+            fontFamily: {
+                'terminal': 'monospace, courier new, courier'
             }
         },
     },
