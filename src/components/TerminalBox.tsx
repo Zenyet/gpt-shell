b7a9ae267520d780c4d50eb85163c6ab39fc67e5
Zenyet <zenyet1998@gmail.com>
2023-06-04 00:54:25 +0800

    terminal backdrop filter.

--- src/components/TerminalBox.tsx
+++ src/components/TerminalBox.tsx
@@ -3,7 +3,7 @@ import {TerminalBottom} from "./TerminalBottom.tsx";
 
 export function TerminalBox() {
     return (
-        <div className='border-[1px] border-ter-border bg-black w-[66%] h-[85%] rounded-xl overflow-hidden'>
+        <div className='border-[1px] border-ter-border bg-[rgba(0,0,0,.7)] backdrop-blur-md transform-gpu w-[66%] h-[85%] rounded-xl overflow-hidden'>
             <TerminalTop/>
             <TerminalBottom/>
         </div>
