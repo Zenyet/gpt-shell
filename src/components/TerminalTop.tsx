5c089af3383609b91623765b0b28247c9af6ad8b
Zenyet <zenyet1998@gmail.com>
2023-06-07 11:11:33 +0800

    adds bottom border.

--- src/components/TerminalTop.tsx
+++ src/components/TerminalTop.tsx
@@ -17,7 +17,7 @@ export function TerminalTop() {
     }
 
     return (
-        <div className='w-[100%] bg-ter-top h-[1.8rem] flex items-center'>
+        <div className='w-[100%] bg-ter-top h-[1.8rem] flex items-center border-b-[1px] border-b-gray-950'>
             <span className='bg-[#ff6057] w-3.5 h-3.5 ml-1.5 rounded-full'></span>
             <span className='bg-[#ffbc2e] w-3.5 h-3.5 ml-2 rounded-full'></span>
             <span onClick={() => handleClick()} className='bg-[#00c840] w-3.5 h-3.5 ml-2 rounded-full'></span>
