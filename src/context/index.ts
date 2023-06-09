7dfb236cc7e235cba7a2a27ee26c68b580769c7d
Zenyet <zenyet1998@gmail.com>
2023-06-09 14:34:32 +0800

    update context.

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,6 +1,8 @@
 import {createContext} from "react";
+import {Config, Operation} from "../types";
 
-export const ModalContext = createContext<{
+export const GlobalContext = createContext<{
     openModal?: () => void
     closeModal?: () => void
+    toggleMode?: (mode: string) => void
 }>({})
