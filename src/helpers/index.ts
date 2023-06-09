b85cd19ea4a0b9ac508d4b610a3f456e3746cdea
Zenyet <zenyet1998@gmail.com>
2023-06-09 14:35:12 +0800

    adds parse fn.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -41,3 +41,55 @@ export function genL_L(date: Date): string {
 
     return `Last login: ${dayOfWeek} ${month} ${day} ${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds} on Chrome`;
 }
+
+
+export async function parse(
+    body: ReadableStream<Uint8Array>,
+    onParse: (piece: string) => void,
+    onDone: (fullText: string, reply?: { message_id: string, conversation_id: string }) => void,
+    mode?: string
+) {
+    const d = new TextDecoder('utf8');
+    const e = new TextEncoder();
+    const reader = await body.getReader();
+    let fullText = '';
+    let message_id = '';
+    let conversation_id = '';
+    while (true) {
+        const {value, done} = await reader.read();
+        if (done) { // stream end
+            onDone(fullText, {message_id, conversation_id});
+            break;
+        } else {
+            const decodedString = d.decode(value);
+            try {
+                if (mode === 'api') {
+                    //fixes string not json-parseable otherwise
+                    let splits: string[] = decodedString.split('data: ');
+                    splits = splits.filter(_ => _ !== '');
+                    splits.forEach(_ => {
+                        const text: string = JSON.parse(_).choices[0].delta?.content || '';
+                        fullText += text;
+                        onParse(fullText);
+                    })
+                } else if (mode === 'chatgpt-reverse') {
+                    //fixes string not json-parseable otherwise
+                    let splits: string[] = decodedString.split('data:');
+                    splits = splits.filter(_ => _ !== '');
+                    splits.forEach(_ => {
+                        const o = JSON.parse(_);
+                        const rawText = o?.message?.content?.parts[0];
+                        if (!message_id || !conversation_id) {
+                            message_id = o?.message?.id;
+                            conversation_id = o?.conversation_id;
+                        }
+                        const text = d.decode(e.encode(rawText));
+                        fullText = text;
+                        onParse(text);
+                    })
+                }
+            } catch { /* empty */
+            }
+        }
+    }
+}
