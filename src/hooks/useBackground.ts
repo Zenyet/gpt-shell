52eb6fb7a6a30612433ff358b552d6e3294a0115
Zenyet <52519506+Zenyet@users.noreply.github.com>
2023-11-20 00:17:50 +0800

    Update useBackground.ts

--- src/hooks/useBackground.ts
+++ src/hooks/useBackground.ts
@@ -3,16 +3,17 @@ import {useEffect, useState} from "react";
 export function useBackground(): string {
     const [bingURL, setURL] = useState<string>('');
     useEffect(() => {
-        fetch('https://proxy.thoughtflow.org/proxy/www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US', {
+        fetch('https://bing.biturl.top/', {
             method: 'GET',
             headers: {
                 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             }
         }).then(resp => resp.json()).then(json => {
-            const baseURL = "https://www.bing.com";
-            const {images: [{url}]} = json;
-            setURL(baseURL + url);
+            //const baseURL = "https://www.bing.com";
+            //const {images: [{url}]} = json;
+            const { url } = json;
+            setURL(url);
         }).catch(err => console.log(err.message))
     }, []);
     return bingURL;
